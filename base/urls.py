4ac85d693927575b41089ac0264db6f90d55099d
CawaTrueYT5 <hlushkosashas@gmail.com>
2025-06-07 17:27:04 +0300

    Improvements

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.base_view, name="base")
+    path("", views.base_view, name="base"),
+    path("novations/", views.NovationsListView.as_view(), name="novations"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Novation
 
 def base_view(request):
     return render(request, "base.html")
+
+class NovationsListView(ListView):
+    model = Novation
+    context_object_name = "novations"
