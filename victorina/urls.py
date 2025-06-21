9782e001f5b3d321f928aadd7b8603d725094cea
CawaTrueYT5 <hlushkosashas@gmail.com>
2025-06-21 17:52:28 +0300

    Some work done

--- victorina/urls.py
+++ victorina/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.VictorinasListView.as_view(), name="victorina_list")
+    path("", views.VictorinasListView.as_view(), name="victorina_list"),
+    #path(<int:pk>/, views., name="victorina_page"),
+    path("a/", views.VictorinaProcessing.as_view(), name="victorina_processing"),
 ]
--- victorina/views.py
+++ victorina/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Victorina
 
 class VictorinasListView(ListView):
     model = Victorina
     context_object_name = "victorinas"
+
+class VictorinaProcessing(DetailView):
+    model = Victorina
+    context_object_name = "victorina"
+    
+    print(Victorina.__str__(Victorina))
