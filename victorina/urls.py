5df27b7f0cbed1375b297987afa5e1994eb5f343
CawaTrueYT5 <hlushkosashas@gmail.com>
2025-07-05 16:31:18 +0300

    Some new improvements

--- victorina/urls.py
+++ victorina/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.VictorinasListView.as_view(), name="victorina_list"),
-    #path(<int:pk>/, views., name="victorina_page"),
-    path("a/", views.VictorinaProcessing.as_view(), name="victorina_processing"),
+    path("<int:pk>/", views.VictorinaDetailView.as_view(), name="victorina_page"),
+    path("<int:pk>/processing/", views.VictorinaProcessing.as_view(), name="victorina_processing"),
 ]
--- victorina/views.py
+++ victorina/views.py
@@ -6,8 +6,10 @@ class VictorinasListView(ListView):
     model = Victorina
     context_object_name = "victorinas"
 
+class VictorinaDetailView(DetailView):
+    model = Victorina
+    context_object_name = "victorina"
+
 class VictorinaProcessing(DetailView):
     model = Victorina
     context_object_name = "victorina"
-    
-    print(Victorina.__str__(Victorina))
